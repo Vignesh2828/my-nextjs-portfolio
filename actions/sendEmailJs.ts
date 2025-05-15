46cff39c7d7d813094d08f05ca59d23f6a1596c6
Vigneshmani28 <vigneshwaran.manivannan@ticktix.com>
2025-05-15 12:11:44 +0530

    email fix

--- actions/sendEmailJs.ts
+++ actions/sendEmailJs.ts
@@ -10,10 +10,6 @@ export const sendEmail = async (values: {
   };
 
   try {
-    if (!process.env.EMAIL_SERVICE_ID || !process.env.EMAIL_TEMPLATE_ID || !process.env.EMAIL_PUBLIC_KEY) {
-      throw new Error("Missing required environment variables for EmailJS.");
-    }
-
     const result = await emailjs.send(
       'service_z5ugceq',
       'template_z4ca7j2',
